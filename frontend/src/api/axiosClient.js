f4f9619a60bf0a587bf643f8503048ae8b77e566
youssefbassem42 <youssefbassem42@gmail.com>
2026-04-24 22:52:19 +0300

    changing urls

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosClient = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
   timeout: 15000,
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true,
