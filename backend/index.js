db2ad13c5d0d7a8d05b6a3187a4b0b7471b07d25
youssefbassem42 <youssefbassem42@gmail.com>
2026-04-24 22:10:33 +0300

    solving production settings

--- backend/index.js
+++ backend/index.js
@@ -18,14 +18,10 @@ const app = express();
 
 fs.mkdirSync(ensureUploadsDir, { recursive: true });
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || "*",
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: "https://management-taskify.vercel.app",
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(passport.initialize());
