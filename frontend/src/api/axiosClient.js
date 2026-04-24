8b5d475f97c49d6f058ef40ba748472c1d86fec7
youssefbassem42 <youssefbassem42@gmail.com>
2026-04-24 20:16:13 +0300

    setting up

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 const axiosClient = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api',
   timeout: 15000,
-  headers: { 'Content-Type': 'application/json' }
+  headers: { 'Content-Type': 'application/json' },
+  withCredentials: true,
 })
 
 axiosClient.interceptors.request.use((config) => {
